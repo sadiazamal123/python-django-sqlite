1c3d22a0e2bc03ff65d83ee1aa7ced20af20d67c
Sadia Zamal <61104968+sadiazamal123@users.noreply.github.com>
2021-12-21 20:39:41 +0600

    urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('project/<str:pk>/', views.project, name="project"),   #Throughing dynamic data
     path('create-project/',views.createProject, name="create-project"),
     path('update-project/<str:pk>/', views.updateProject, name = "update-project"),
-    path('delete-project/<str:pk>/', views.deleteProject, name = "delete-project"),
+    path('delete-project/<str:pk>/', views.deleteProject, name = "delete-project"), 
 
 ]
