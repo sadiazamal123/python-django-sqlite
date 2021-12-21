f42a65645b74cee9fdcff889c1d85d119f432860
Sadia Zamal <61104968+sadiazamal123@users.noreply.github.com>
2021-12-21 20:47:59 +0600

    users forms.py

--- users/forms.py
+++ users/forms.py
@@ -14,7 +14,7 @@ class CustomUserCreationForm(UserCreationForm):
         }
 
     def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs) 
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
