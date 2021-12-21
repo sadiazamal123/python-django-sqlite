5dac37bce2101d2ff0379c457af08f43a79fa9f4
Sadia Zamal <61104968+sadiazamal123@users.noreply.github.com>
2021-12-21 20:38:27 +0600

    models update

--- projects/models.py
+++ projects/models.py
@@ -48,4 +48,4 @@ class Tag(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return self.name 
+        return self.name  
